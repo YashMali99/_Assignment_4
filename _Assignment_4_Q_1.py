77f191c600e44d4eb2076ec3a23c0a78e9ceaf3f
YashMali99 <112956704+YashMali99@users.noreply.github.com>
2022-10-02 19:26:40 +0530

    Answer of question no.1

---
+++ _Assignment_4_Q_1.py
@@ -0,0 +1,19 @@
+
+
+# Q.1) Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+
+# sample input: 10
+
+# sample output: 35
+
+
+
+
+
+v = (lambda a : a + 25)
+print(v(10))
+
+
+
+
